362324936f3f095c4dca8b8d418793c905f9e342
Jmestrelozano <jmestrelozano@gmail.com>
2025-02-22 11:58:19 -0500

    ✨ feat: Implement GradientBackground component with linear gradient and customizable children

---
+++ src/presentation/components/common/gradientBackground/GradientBackground.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import LinearGradient from 'react-native-linear-gradient';
+
+import {gradientBackgroundStyles} from './gradientBackground.styles';
+
+import { GradientBackgroundProps } from '../../../interfaces/components/common/gradientBackground/gradientBackground.interface';
+
+export const GradientBackground: React.FC<GradientBackgroundProps> = ({
+  children,
+}) => {
+  return (
+    <LinearGradient
+      colors={['#2d1309', '#5a2e12', '#9b4f1e', '#d87a3a', '#f0b48a']}
+      start={{x: 0.2, y: 0}}
+      end={{x: 0.8, y: 1}}
+      style={gradientBackgroundStyles.gradientBackground}>
+      {children}
+    </LinearGradient>
+  );
+};
