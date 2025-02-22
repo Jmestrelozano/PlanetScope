f4c8588645f17cba78de112b7c910152e380c158
Jmestrelozano <jmestrelozano@gmail.com>
2025-02-22 11:53:05 -0500

    🌈 style: Define searchInputStyles with input style properties for the SearchInput component

---
+++ src/presentation/components/ui/inputs/searchInput/searchInput.styles.ts
@@ -0,0 +1,11 @@
+import {StyleProp, TextStyle} from 'react-native';
+
+export const searchInputStyles: {[key: string]: StyleProp<TextStyle>} = {
+  input: {
+    backgroundColor: 'white',
+    borderRadius: 10,
+    paddingLeft: 10,
+    fontFamily: 'OpenSans-Regular',
+    fontWeight: '400',
+  },
+};
