1388205da4c57857a79a6615e1a091bb9dcd18f7
Jmestrelozano <jmestrelozano@gmail.com>
2025-02-22 11:53:58 -0500

    🌈 style: Define headerStyles with title and subtitle styles for the HeaderTitle component

---
+++ src/presentation/components/ui/headers/headers.styles.ts
@@ -0,0 +1,19 @@
+import { StyleProp, TextStyle } from 'react-native';
+import { RFPercentage } from 'react-native-responsive-fontsize';
+
+export const headerStyles: { [key: string]: StyleProp<TextStyle> } = {
+  title: {
+    fontSize: RFPercentage(5),
+    fontFamily: 'LilitaOne-Regular',
+    fontWeight: '400',
+    color: 'white',
+    textAlign: 'center',
+  },
+  subtitle: {
+    fontSize: RFPercentage(3),
+    fontFamily: 'LilitaOne-Regular',
+    fontWeight: '300',
+    color: '#ccc',
+    textAlign: 'center',
+  },
+};
