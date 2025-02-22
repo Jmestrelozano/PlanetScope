eee791efacb56c8792e6f74003e148185263dd0b
Jmestrelozano <jmestrelozano@gmail.com>
2025-02-22 12:01:00 -0500

    ✨ feat: Define planetImages object to map planet names to their corresponding image assets

---
+++ src/config/planetImages.ts
@@ -0,0 +1,10 @@
+export const planetImages = {
+  uranus: require('../assets/images/uranus.png'),
+  neptune: require('../assets/images/neptune.png'),
+  jupiter: require('../assets/images/jupiter.png'),
+  mars: require('../assets/images/mars.png'),
+  mercure: require('../assets/images/mercure.png'),
+  saturne: require('../assets/images/saturne.png'),
+  terre: require('../assets/images/terre.png'),
+  venus: require('../assets/images/venus.png'),
+};
