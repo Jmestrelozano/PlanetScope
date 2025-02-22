a8612c216b69f82900fdaa867261b23860124e4a
Jmestrelozano <jmestrelozano@gmail.com>
2025-02-22 11:55:28 -0500

    🌈 style: Define customTabButtonStyles with styling for the CustomTabButton component

---
+++ src/presentation/components/navigation/customTabButton.styles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const customTabButtonStyles = StyleSheet.create({
+  tabButton: {
+    height: 70,
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 25,
+    padding: 10,
+  },
+});
