866d8aa9ba281c63eb283f589219cc7bd17b37b2
Jmestrelozano <jmestrelozano@gmail.com>
2025-02-23 15:04:34 -0500

    🦄 refactor: Rename name of the syle

--- src/presentation/components/ui/inputs/searchInput/SearchInput.tsx
+++ src/presentation/components/ui/inputs/searchInput/SearchInput.tsx
@@ -2,19 +2,19 @@ import React, {FC} from 'react';
 import {TextInput} from 'react-native';
 
 import {SearchInputProps} from '../../../../interfaces/components/ui/inputs/searchInput/searchInput.interface';
-import {searchInputStyles} from './searchInput.styles';
+import { styles } from './styles';
 
 export const SearchInput: FC<SearchInputProps> = ({
   value,
   onChangeText,
   placeholder = 'Search planets...',
-  style = searchInputStyles.input,
+  style
 }) => {
   return (
     <TextInput
       placeholder={placeholder}
       value={value}
-      style={[searchInputStyles.input, style]}
+      style={[styles.input, style]}
       onChangeText={onChangeText}
     />
   );
