a64dada59c5efb3dd02b9a39883d199b96783939
Jmestrelozano <jmestrelozano@gmail.com>
2025-02-23 14:52:53 -0500

    🦄 refactor: Update HomeScreen component to include search functionality and sorting controls

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,40 +1,64 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {View} from 'react-native';
 
 import {SearchInput} from '../../components/ui/inputs/searchInput/SearchInput';
-import {HeaderTitle} from '../../components/ui/headers/HeaderTitle';
+import {HeaderTitle} from '../../components/ui/headers/headerTitle/HeaderTitle';
 import {GradientBackground} from '../../components/common/gradientBackground/GradientBackground';
 import {ListItems} from '../../components/common/listItem/ListItems';
 import {ListEmpty} from '../../components/common/listItem/ListEmpty';
+import {SortControls} from '../../components/common/sort/SortControls';
+import {ErrorBoundary} from '../../components/common/errorBoundary/ErrorBoundary';
 
 import {useStore} from '../../../state/store';
-import {homeScreenStyles} from './homeScreen.styles';
+import {styles} from './styles';
 
 const HomeScreen = () => {
-  const {planets} = useStore(store => store);
+  const {
+    planetState: {planets, errorMessage, hasError},
+    sortPlanets,
+  } = useStore(store => store);
   const [search, setSearch] = useState('');
+
+  const isEmptyList = [].length === 0;
+  const emptyMessage = isEmptyList
+    ? {title: 'No planets available', description: 'Try again later'}
+    : {title: 'No results found', description: 'Try another search'};
 
   return (
     <GradientBackground>
-      <View style={homeScreenStyles.container}>
+      <View style={styles.container}>
         <HeaderTitle title="PLANETS" />
-        <View style={homeScreenStyles.contentWrapper}>
-          <SearchInput
-            value={search}
-            onChangeText={setSearch}
-            style={{backgroundColor: 'white'}}
-          />
-          <ListItems
-            data={planets}
-            search={search}
-            ListEmptyComponent={
-              <ListEmpty
-                title="Sorry"
-                description="There is no related search"
+        <ErrorBoundary hasError={hasError} errorMessage={errorMessage}>
+          <View style={styles.contentWrapper}>
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                gap: 10,
+              }}>
+              <SearchInput
+                value={search}
+                onChangeText={setSearch}
+                style={{backgroundColor: 'white', width: '70%'}}
               />
-            }
-          />
-        </View>
+              <SortControls
+                onSortAsc={() => sortPlanets('asc')}
+                onSortDesc={() => sortPlanets('desc')}
+              />
+            </View>
+
+            <ListItems
+              data={planets}
+              search={search}
+              ListEmptyComponent={
+                <ListEmpty
+                  title={emptyMessage.title}
+                  description={emptyMessage.description}
+                />
+              }
+            />
+          </View>
+        </ErrorBoundary>
       </View>
     </GradientBackground>
   );
