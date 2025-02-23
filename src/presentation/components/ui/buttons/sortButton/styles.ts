6942ccbd71eb9bbf906b4398fbccfcf761584d32
Jmestrelozano <jmestrelozano@gmail.com>
2025-02-23 15:06:37 -0500

    🌈 style: Add button styles with borderWidth, borderColor, padding, and borderRadius properties

---
+++ src/presentation/components/ui/buttons/sortButton/styles.ts
@@ -0,0 +1,10 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  button: {
+    borderWidth: 1,
+    borderColor: 'white',
+    padding: 8,
+    borderRadius: 10,
+  },
+});
