7c04dacc7773c272239ff7703bd3a263d6909d9e
Jmestrelozano <jmestrelozano@gmail.com>
2025-02-23 15:11:38 -0500

    🦄 refactor: Modify name

--- src/presentation/components/common/customImage/styles.ts
+++ src/presentation/components/common/customImage/styles.ts
@@ -1,6 +1,6 @@
 import {StyleSheet} from 'react-native';
 
-export const customImageStyles = StyleSheet.create({
+export const styles = StyleSheet.create({
   image: {
     width: 80,
     height: 80,
