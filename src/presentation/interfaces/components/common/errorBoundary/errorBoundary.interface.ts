e3d044122555959c3cdb168bf29f48c348e7d6ed
Jmestrelozano <jmestrelozano@gmail.com>
2025-02-23 18:28:55 -0500

    🦄 refactor: Add refreshRequest

--- src/presentation/interfaces/components/common/errorBoundary/errorBoundary.interface.ts
+++ src/presentation/interfaces/components/common/errorBoundary/errorBoundary.interface.ts
@@ -3,6 +3,7 @@ import { ReactNode } from "react";
 export interface ErrorBoundaryProps {
   hasError?: boolean;
   errorMessage?: string;
+  refreshRequest?: () => void;
   children: ReactNode;
 }
 
