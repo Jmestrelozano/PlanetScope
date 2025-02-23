bb9867a9c79ed98b026f66ee91c7c43537bcd592
Jmestrelozano <jmestrelozano@gmail.com>
2025-02-23 15:10:29 -0500

    🦄 refactor: Rename name of the style

--- src/presentation/components/common/gradientBackground/GradientBackground.tsx
+++ src/presentation/components/common/gradientBackground/GradientBackground.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import LinearGradient from 'react-native-linear-gradient';
 
-import {gradientBackgroundStyles} from './gradientBackground.styles';
-
 import { GradientBackgroundProps } from '../../../interfaces/components/common/gradientBackground/gradientBackground.interface';
+
+import {styles} from './styles';
 
 export const GradientBackground: React.FC<GradientBackgroundProps> = ({
   children,
@@ -13,7 +13,7 @@ export const GradientBackground: React.FC<GradientBackgroundProps> = ({
       colors={['#2d1309', '#5a2e12', '#9b4f1e', '#d87a3a', '#f0b48a']}
       start={{x: 0.2, y: 0}}
       end={{x: 0.8, y: 1}}
-      style={gradientBackgroundStyles.gradientBackground}>
+      style={styles.gradientBackground}>
       {children}
     </LinearGradient>
   );
