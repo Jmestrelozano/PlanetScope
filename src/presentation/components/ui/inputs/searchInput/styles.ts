0ff8317ffe9fb9c905807d057b1e5f2cd70730ac
Jmestrelozano <jmestrelozano@gmail.com>
2025-02-23 15:04:00 -0500

    🦄 refactor: Modify name

--- src/presentation/components/ui/inputs/searchInput/styles.ts
+++ src/presentation/components/ui/inputs/searchInput/styles.ts
@@ -1,6 +1,6 @@
 import {StyleProp, TextStyle} from 'react-native';
 
-export const searchInputStyles: {[key: string]: StyleProp<TextStyle>} = {
+export const styles: {[key: string]: StyleProp<TextStyle>} = {
   input: {
     backgroundColor: 'white',
     borderRadius: 10,
