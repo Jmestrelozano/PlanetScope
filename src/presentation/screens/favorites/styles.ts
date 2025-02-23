7103c6625dec0e10623284e1a2dd3ed12192f023
Jmestrelozano <jmestrelozano@gmail.com>
2025-02-23 14:54:19 -0500

    🦄 refactor:  Modify name

--- src/presentation/screens/favorites/styles.ts
+++ src/presentation/screens/favorites/styles.ts
@@ -1,15 +1,9 @@
 import {StyleSheet} from 'react-native';
 
-export const homeScreenStyles = StyleSheet.create({
+export const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingVertical: '15%',
-  },
-  contentWrapper: {
-    paddingTop: 30,
-    paddingHorizontal: 20,
-    flexDirection: 'column',
-    gap: 20,
   },
   listContent: {
     gap: 40,
